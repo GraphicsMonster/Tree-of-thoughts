0466b9945c16c3c0a04bc018fa050173125c2ef6
Saksham Gupta <www.rameshgupta67@gmail.com>
2023-09-30 23:31:49 +0530

    a few more adjustments. Still in the process of developing a comprehensive idea

--- tree.py
+++ tree.py
@@ -1,25 +1,23 @@
 import torch
 import torch.nn as nn
-from model import Embedding_model, MultiHeadAttention, BigramLanguageModel
+from model import BigramLanguageModel
 
 class TreeOfThoughts(nn.Module):
-    def __init__(self, bigram_model, num_branches=3, max_seq_len=20):
+    def __init__(self, num_branches, max_token_sequence_length):
         super(TreeOfThoughts, self).__init__()
         self.bigram_model = BigramLanguageModel()
         self.num_branches = num_branches
-        self.max_seq_len = max_seq_len
+        self.mtsl = max_token_sequence_length
+        self.branch_seed = [None] * self.num_branches
+        self.tree = [[None for _ in range(self.mtsl)] for _ in range(self.num_branches)]
+    
+    def forward(self, seed):
+        for _ in range(self.num_branches):
+            self.branch_seed[_] = self.bigram_model(seed)
 
-    def forward(self, seed_token):
-        # Generate num_branches sequences of tokens
-        branches = []
         for i in range(self.num_branches):
-            sequence = [seed_token]
-            for j in range(self.max_seq_len):
-                # Predict the next token using the bigram model
-                next_token = self.bigram_model.forward(sequence)
-                if next_token == '<EOS>':
-                    break
-                sequence.append(next_token)
-            branches.append(sequence)
-        return branches
+            for j in range(self.mtsl):
+                self.tree[i][j] = self.bigram_model(self.branch_seed[i][j])
+
+        return self.tree
 
